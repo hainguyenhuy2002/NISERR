323824c7254e744e476a5a84cd4a58a5b318b274
hainguyenhuy2002 <81941497+hainguyenhuy2002@users.noreply.github.com>
2023-06-13 22:56:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,13 +50,9 @@ def main():
         n_node = 43098
     elif opt.dataset == 'yoochoose1_64' or opt.dataset == 'yoochoose1_4':
         n_node = 37484
-    elif opt.dataset == '/kaggle/input/withoutactionnot12':
-        n_node = 84509
-        
-    elif opt.dataset =='/kaggle/input/largerthan7':
-        n_node = 43610
-    elif opt.dataset =='/kaggle/input/withoumappingaction':
-        n_node = 110631
+    elif opt.dataset =='/kaggle/input/userbrowsing234':
+        n_node = 12012
+    
 
     model = trans_to_cuda(SessionGraph(opt, n_node))
 
